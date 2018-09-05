a092753288b5c5a75fc55794ba0d5a122cd23b1c
inbae <cleverdevk@gmail.com>
2018-09-05 11:27:52 +0900

    modify several errors

--- main_mix.py
+++ main_mix.py
@@ -41,7 +41,7 @@ def isExceedtime(input_time, threshold): #determine whether input_time exceed th
 def inOutingSchedule(input_time, start_time, end_time): #determine whether input_time is in the term between outing schedule
     if(input_time>=start_time and input_time<=end_time):
         return True
-    else
+    else:
         return False
 
 
@@ -59,7 +59,7 @@ def UPDATEorNOT(fcm,input_selectedPain,input_selectedOut):
         Alarmlist.append(fcm.get("/DOSE/" + pains[i], None))
         Alarmlist[i].sort()
     for i in range(0, len(outings)):  # init Outinglist by getting list from database
-        Outinglist.append(fcm.get("/OUTING" + outings[i], None))
+        Outinglist.append(fcm.get("/OUTING/" + outings[i], None))
     minimumTime = Alarmlist[0][0]  # To search minimumTime of Alarm
     selectedPain = pains[0]
 
@@ -101,7 +101,7 @@ if __name__ == '__main__':
             Alarmlist.append(fcm.get("/DOSE/" + pains[i], None))
             Alarmlist[i].sort()
          for i in range(0, len(outings)): #init Outinglist by getting list from database
-            Outinglist.append(fcm.get("/OUTING" + outings[i], None))
+            Outinglist.append(fcm.get("/OUTING/" + outings[i], None))
          minimumTime = Alarmlist[0][0] #To search minimumTime of Alarm
          selectedPain = pains[0]
          selectedPainNumber = 0
@@ -109,6 +109,7 @@ if __name__ == '__main__':
          minimumOut = Outinglist[0][0] #To search minimumTime of Outing
          selectedOut = outings[0]
          selectedOutNumber = 0
+         outingFlag = False
 
          for i in range(0, len(pains)): #Searching minimum Alarm Time
              if Alarmlist[i][0] <= minimumTime:
@@ -127,34 +128,39 @@ if __name__ == '__main__':
          currentOutingEnd = Outinglist[selectedOutNumber][1][2:] #Slicing String of Outing
 
         #################### for give a different portion of medicine ##############################
-         outingStackCount = []
-
-         cnt = 1
+         outingStackCount = list()
+         outingStackCount.append(0)
          outingStackCount.append(1)
-         for i in range(1,len(Alarmlist[selectedOutNumber])):
-             outingStackCount.append(0)
-             if (inOutingSchedule(Alarmlist[selectedOutNumber][i], currentOutingStart, currentOutingEnd)):
+         cnt = 1
+         for i in range(2,len(Alarmlist[selectedOutNumber])+1):
+             outingStackCount.append(1)
+             if (inOutingSchedule(Alarmlist[selectedPainNumber][i-1], currentOutingStart, currentOutingEnd)):
                  if(outingStackCount[i-1]!=0):
                     outingStackCount[i-1] += 1
-                    outingStackCount = 0
+                    outingStackCount[i] = 0
+                    outingFlag = True
              else:
-                 outingStackCount[i] = 1
+                 outingStackCount[i-1] = 1
         ##############################################################################################
 
-         print("Selected Pain & Time : ", selectedPain, minimumTime, "Selected Outing Schedule : ", selectedOut) #Log
+         print("[SYSYEM] Selected Pain & Time : ", selectedPain, minimumTime) #Log
+         print("[SYSTEM] Selected Outing Schedule : ", selectedOut,currentOutingStart,currentOutingEnd, outingStackCount[1:]) #Log
 
          for i in range(0, len(Alarmlist[selectedPainNumber])): # Main Algorithm
 
              #--------------------------- time check mode --------------------------------------------
              if(mode): #time check mode
+                print("#########################Time Check mode#########################")
                 # to check the new data in database
                 if(UPDATEorNOT(fcm,selectedPain,selectedOut)):
+                    print("[SYSTEM] Need to Update!, Data will be updated")
                     break
                 nextAlarmTime = Alarmlist[selectedPainNumber][0]
-                if(isEqualtime(nextAlarmTime)): # when the time is to provide a medicine
-                    for j in range(0,outingStackCount[i]):
+                print("[SYSTEM] Current Alarm Time : ",nextAlarmTime)
+                if(isEqualtime(nextAlarmTime) or True): # when the time is to provide a medicine/ in actual use, remove "or True"
+                    for j in range(0,outingStackCount[i+1]):
                         sm.step()
-                        sleep(10)
+                        time.sleep(10)
                     mode = False
                     Alarmlist[selectedPainNumber].remove(nextAlarmTime)
                     #refresh the database
@@ -165,55 +171,60 @@ if __name__ == '__main__':
                     pass
              #-------------------------------- distance check mode --------------------------------------
              else:
-
-                for j in range(0,len(outingStackCount)):
-                    if(outingStackCount[i]!=1):
-                        result2 = fcm.patch("/OUTING",{selectedOut : None}) # i don't know that it works.
-                        break
-
-                if(us.getDistance()>threshold):
-                    #result = push_service.notify_single_device(registration_id=getToken(fcm),message_body="user takes a medicine.")
-                    print("[SYSTEM] taking Alarm is generated in andrioid Device")
-                    history = fcm.get("/HISTORY/"+selectedPain,None)
-                    if history == None:
-                        history = []
-                    history.append(nextAlarmTime+"#1#"+selectedPain)
-                    result = fcm.patch("/",{"/HISTORY" : history})
-                    time.sleep(5)
-                    #add to delete from database
-                else:
-                    if(AlarmCount == 3):
-                        if(isExceedtime(nextAlarmTime,5)): # if the medicine is not brought for 5 mintues
-                            #result = push_service.notify_single_device(registration_id=getToken(fcm),message_body="user does not take a medicine.")
-                            print("[SYSTEM] 1 not taking Alarm is generated in android Device")
-                            AlarmCount -= 1
-                            time.sleep(5)
-                        else:
-                            pass
-                    elif(AlarmCount == 2) :
-                        if(isExceedtime(nextAlarmTime,10)):
-                            #result = push_service.notify_single_device(registration_id=getToken(fcm),message_body="user does not take a medicine.")
-                            print("[SYSTEM] 2 not taking Alarm is generated in android Device")
-                            AlarmCount -= 1
-                            time.sleep(5)
-                    elif(AlarmCount == 1) :
-                        if(isExceedtime(nextAlarmTime,15)):
-                            #result = push_service.notify_single_device(registration_id=getToken(fcm),message_body="user does not take a medicine.")
-                            print("[SYSTEM] 3 not taking Alarm is generated in android Device")
-                            AlarmCount -= 1
-                            time.sleep(5)
+                print("#########################Distance Check Mode#################################")
+                while True:
+                    if(outingFlag):
+                        print("[SYSTEM] Remove Outing Schedule")
+                        esult2 = fcm.patch("/OUTING",{selectedOut : None}) # i don't know that it works.
+                    if(False):
+                    #if(us.getDistance()>threshold and False): # in actual use, remove "and False"
+                        #result = push_service.notify_single_device(registration_id=getToken(fcm),message_body="user takes a medicine.")
+                        print("[SYSTEM] taking Alarm is generated in andrioid Device")
+                        history = fcm.get("/HISTORY/"+selectedPain,None)
+                        if history == None:
+                            history = []
+                        history.append(nextAlarmTime+"#1#"+selectedPain)
+                        result = fcm.patch("/",{"/HISTORY" : history})
+                        time.sleep(5)
+                        #add to delete from database
                     else:
-                        if(isExceedtime(nextAlarmTime,15)):
-                            #result = push_service.notify_single_device(registration_id=getToken(fcm),message_body="user does not take a medicine.")
-                            print("[SYSTEM] really not taking Alarm is generated in android Device")
-                            history = fcm.get("/HISTORY",None)
-                            if history == None:
-                                history = []
-                            history.append(nextAlarmTime+"#0#"+selectedPain)
-                            result = fcm.patch("/",{"/HISTORY" : history})
-                            AlarmCount = 3
-                            mode = True
-                            time.sleep(5)
+                        if(AlarmCount == 3):
+                            if(isExceedtime(nextAlarmTime,5)): # if the medicine is not brought for 5 mintues
+                                #result = push_service.notify_single_device(registration_id=getToken(fcm),message_body="user does not take a medicine.")
+                                print("[SYSTEM] 1 not taking Alarm is generated in android Device")
+                                AlarmCount -= 1
+                                time.sleep(5)
+                            else:
+                                pass
+                        elif(AlarmCount == 2) :
+                            if(isExceedtime(nextAlarmTime,10)):
+                                #result = push_service.notify_single_device(registration_id=getToken(fcm),message_body="user does not take a medicine.")
+                                print("[SYSTEM] 2 not taking Alarm is generated in android Device")
+                                AlarmCount -= 1
+                                time.sleep(5)
+                            else:
+                                pass
+                        elif(AlarmCount == 1) :
+                            if(isExceedtime(nextAlarmTime,15)):
+                                #result = push_service.notify_single_device(registration_id=getToken(fcm),message_body="user does not take a medicine.")
+                                print("[SYSTEM] 3 not taking Alarm is generated in android Device")
+                                AlarmCount -= 1
+                                time.sleep(5)
+                            else:
+                                pass
+                        else:
+                            if(isExceedtime(nextAlarmTime,15)):
+                                #result = push_service.notify_single_device(registration_id=getToken(fcm),message_body="user does not take a medicine.")
+                                print("[SYSTEM] really not taking Alarm is generated in android Device")
+                                history = fcm.get("/HISTORY",None)
+                                if history == None:
+                                    history = []
+                                history.append(nextAlarmTime+"#0#"+selectedPain)
+                                result = fcm.patch("/",{"/HISTORY" : history})
+                                AlarmCount = 3
+                                mode = True
+                                time.sleep(5)
+                                break
                 
 
                     
